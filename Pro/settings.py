5d279a6b5890f427aa724907eb1791a633209182
Jcan20 <137077702+Jcan20@users.noreply.github.com>
2025-02-10 22:02:01 -0500

    Update settings.py

--- Pro/settings.py
+++ Pro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(z3nwdue)-amu7wb!_*l$nbo$9x-)3@)o6ffq(a+8$5!%gzox$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pruebafinalAbraham.onrender.com']
 
 
 # Application definition
